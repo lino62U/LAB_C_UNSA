388844e8ea7a83a0910f2aa30ddcc039df89da42
Avelino Lupo <103952759+lino62U@users.noreply.github.com>
2022-04-22 14:28:15 -0500

    Update Ejercicio01.cpp

--- Laboratorio_01/Ejercicio01.cpp
+++ Laboratorio_01/Ejercicio01.cpp
@@ -5,10 +5,11 @@ using namespace std;
 /*
 1. Escriba un código que solicite al usuario ingresar dos números enteros y que muestre
 el producto de ambos.
-/*
+*/
 
 int main(){
     int a,b; 
     cout<<"Ingrese dos numeros: ";cin>>a>>b; 
     cout<<"El producto de ambos es: "<<a*b<<endl;
+    return 0;
 }
