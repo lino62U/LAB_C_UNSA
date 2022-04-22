2137be1c2d732b988cbff8e6207754f4637b7a92
Avelino Lupo <103952759+lino62U@users.noreply.github.com>
2022-04-22 13:58:31 -0500

    Update Ejercicio05.cpp

--- Laboratorio_01/Ejercicio05.cpp
+++ Laboratorio_01/Ejercicio05.cpp
@@ -9,8 +9,12 @@ using namespace std;
 /*
 5. Elabore un código que reciba como entrada una secuencia de caracteres que contiene
 un numero flotante y retorne el número redondeado.
-
+/*
 int main(){
-
+    
+    float n;
+    cout<<"N? ";cin>>n;
+    cout<<"\nNumero redondeado: "<<round(n)<<endl;
+    return 0;
 }
-*/
+
