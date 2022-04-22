ff45fba47c8a3ccecbf01e5d2ef8e3d949e485d3
Avelino Lupo <103952759+lino62U@users.noreply.github.com>
2022-04-22 13:27:42 -0500

    Create Ejercicio05.cpp

---
+++ Laboratorio_01/Ejercicio05.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+#include <string.h>
+#include <fstream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+
+/*
+5. Elabore un código que reciba como entrada una secuencia de caracteres que contiene
+un numero flotante y retorne el número redondeado.
+
+int main(){
+
+}
+*/
