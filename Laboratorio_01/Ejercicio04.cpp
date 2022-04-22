ecc5b70dbb98976ab467b12ee77609a21fba7dc8
Avelino Lupo <103952759+lino62U@users.noreply.github.com>
2022-04-22 13:26:50 -0500

    Create Ejercicio04.cpp

---
+++ Laboratorio_01/Ejercicio04.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+#include <string.h>
+#include <fstream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+
+/*
+4. Escriba un código que solicite una cantidad de minutos específica y muestre como
+resultado la hora y fecha resultante tomando como referencia la hora y fecha actual y
+restarle el tiempo indicado.
+
+
+int main(){
+    int min;
+    cout<<"Ingrese minutos: ";cin>>min;
+
+
+    return 0;
+}
+*/
