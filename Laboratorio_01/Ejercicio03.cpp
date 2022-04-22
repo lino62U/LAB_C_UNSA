348e5257428163c396a82449f87494a10560abcb
Avelino Lupo <103952759+lino62U@users.noreply.github.com>
2022-04-22 14:27:48 -0500

    Update Ejercicio03.cpp

--- Laboratorio_01/Ejercicio03.cpp
+++ Laboratorio_01/Ejercicio03.cpp
@@ -7,27 +7,40 @@ using namespace std;
 cadena), solicite un tiempo en minutos a agregar, y retorne la hora de finalización (el
 formato de salida debe de estar en AM o PM según corresponda).
 
-/*
+*/
+string amPM (int hora){
+    if(hora>11)
+        return "pm";
+    else 
+        return "am";
+}
 
 int main(){
     string hora;
     string min;
-    int h, m;
+    int h, m, nueva;
+    int horaaumentar, minutoaumentar;
     cout<<"Ingrese la hora en formato HH:MM? ";getline(cin,hora);
     cout<<"Minutos a agregar: ";getline(cin, min);
+    
     h=stoi(hora.substr(0,2));  //TRANSFORMA EL STRING EN ENTERO
     m=stoi(hora.substr(3,2));
 
     // MANO AQUI PONES EL CODIGO DE AUMENTAR LA HORA
     if((m + stoi(min))>60){
-        h=h+stoi(min)/60;
-        //Primer caso
-       
         
+        if(stoi(min)>60){
+            horaaumentar=stoi(min)/60;
+            minutoaumentar=stoi(min)%60;
+            h=h+horaaumentar;
+            m=m+minutoaumentar;
+            cout<<"Nueva hora: "<<h<<":"<<m<<endl;
+
+        }  
+    }
+    else{
+        cout<<"Nueva hora: "<<hora.substr(0,2)<<":"<<stoi(hora.substr(3,2))+stoi(min)<<endl;
     }
 
-    cout<<"Nueva hora: "<<hora.substr(0,2)<<":"<<stoi(hora.substr(3,2))+stoi(min)<<endl;
+    
 }
-/*
-INCOMPLETO
-*/
