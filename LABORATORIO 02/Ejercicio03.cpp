926ac6cb25dbf00ff4619ab50ba0361ef12918b5
Avelino Lupo <103952759+lino62U@users.noreply.github.com>
2022-04-28 15:56:49 -0500

    Create Ejercicio03.cpp

---
+++ LABORATORIO 02/Ejercicio03.cpp
@@ -0,0 +1,10 @@
+/*
+3. Escribir un programa que visualice en pantalla los números múltiplos de 5
+comprendidos entre 1 y 100.
+*/
+#include<iostream>
+using namespace std;
+int main(){
+  for(int i=10;i<100;i+=5){cout<<i<<" ";}
+  return 0;
+}
