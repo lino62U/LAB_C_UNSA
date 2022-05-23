fdaddf7eed96c5f653b75ac69c243e2155b7f410
Avelino Lupo <103952759+lino62U@users.noreply.github.com>
2022-05-22 21:17:02 -0500

    Create main.cpp

---
+++ LAB07_GRUPO_C_20212162_EJECUTABLE_AVELINO_LUPO/Ejercicio03/main.cpp
@@ -0,0 +1,19 @@
+#include "iostream"
+#include "ProductoBancario.h"
+#include "Prestamo.h"
+#include "Cuenta.h"
+#include "CuentaJoven.h"
+#include "Hipoteca.h"
+#include "string.h"
+
+using namespace std;
+
+int main(){
+
+    //CuentaJoven Obj2;
+    Hipoteca Obj3("Raul",200.34,6);
+    Obj3.imprimir();
+   
+    return 0;
+    system("pause");
+}
