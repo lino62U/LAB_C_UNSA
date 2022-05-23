ac08ab210319d2b75ff7d8f783be6adb576f408b
Avelino Lupo <103952759+lino62U@users.noreply.github.com>
2022-05-22 21:18:15 -0500

    Create main.cpp

---
+++ LAB07_GRUPO_C_20212162_EJECUTABLE_AVELINO_LUPO/Ejercicio04/main.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+#include "ClaseDisco.h"
+#include "ClaseMultimedia.h"
+
+
+using namespace std;
+
+int main(){
+
+    ClaseDisco disco("Rock","Nirvana");
+    disco.Imprimir();
+    return 0;
+    system("pause");
+}
