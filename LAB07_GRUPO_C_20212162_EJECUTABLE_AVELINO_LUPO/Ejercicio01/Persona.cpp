7c4f275ea518964b8579daafd194bb62a9a4b42e
Avelino Lupo <103952759+lino62U@users.noreply.github.com>
2022-05-22 21:16:13 -0500

    Add files via upload

---
+++ LAB07_GRUPO_C_20212162_EJECUTABLE_AVELINO_LUPO/Ejercicio01/Persona.cpp
@@ -0,0 +1,13 @@
+#include "Persona.h"
+#include "stdfix.h"
+using namespace std;
+
+Persona:: Persona (){}
+Persona::~Persona(){}
+void Persona::setEdad(int _edad){
+    edad=_edad;
+}
+
+void Persona::setNombre(string _nombre){
+    nombre=_nombre;
+}
