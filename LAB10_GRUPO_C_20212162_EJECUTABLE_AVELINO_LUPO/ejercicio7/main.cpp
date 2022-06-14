a23a54d6a594d28fa9aa054f07c421519afb2b5a
Avelino Lupo <103952759+lino62U@users.noreply.github.com>
2022-06-14 01:33:26 -0500

    Update main.cpp

--- LAB10_GRUPO_C_20212162_EJECUTABLE_AVELINO_LUPO/ejercicio7/main.cpp
+++ LAB10_GRUPO_C_20212162_EJECUTABLE_AVELINO_LUPO/ejercicio7/main.cpp
@@ -4,18 +4,18 @@
 using namespace std;
 
 
-int main()
-{
+int main(){
+    
     srand(time(NULL));
     unsigned t0, t1;
     Lista* a = NULL;
-    for (int i = 0; i < 10; i++){
+    for (int i = 0; i < 10000; i++){
         a->ingresarDato(a, 1+rand()%100);
     }
     cout<<"\nNueva lista"<<endl;
     a->printList(a);
     t0=clock();   
-         a->quickSort(&a);
+         a->quickSort(a);
 
     t1 = clock();
     cout<<"\n\nNueva lista ordenadada"<<endl;
@@ -25,21 +25,5 @@ int main()
     cout << "Execution Time : " << time << endl;
  
     return 0;
-}
-
-
-/*
-int main(){
-    Lista *lista = NULL;
-    int n;
-    srand(time(NULL));
-    for(int i=0; i<5; i++){
-        lista->ingresarDato(lista,1+rand()%100);
-    }
-    cout<<"\nLista enlazada: "<<endl;
-    lista->print(lista);
-    
-   
-    return 0;
     system("pause");
-}*/
+}
