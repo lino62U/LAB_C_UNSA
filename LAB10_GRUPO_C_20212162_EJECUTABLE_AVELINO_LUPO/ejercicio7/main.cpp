b183f3ea906f55a2a7c8a512575c0602b8825977
Avelino Lupo <103952759+lino62U@users.noreply.github.com>
2022-06-14 08:32:32 -0500

    Update main.cpp

--- LAB10_GRUPO_C_20212162_EJECUTABLE_AVELINO_LUPO/ejercicio7/main.cpp
+++ LAB10_GRUPO_C_20212162_EJECUTABLE_AVELINO_LUPO/ejercicio7/main.cpp
@@ -10,19 +10,25 @@ int main(){
     unsigned t0, t1;
     Lista* a = NULL;
     for (int i = 0; i < 10000; i++){
-        a->ingresarDato(a, 1+rand()%100);
+        a->ingresarDato(a, 1+rand()%200);
     }
-    cout<<"\nNueva lista"<<endl;
-    a->printList(a);
-    t0=clock();   
-         a->quickSort(a);
-
-    t1 = clock();
-    cout<<"\n\nNueva lista ordenadada"<<endl;
-    a->printList(a);
+    // cout<<"\nNueva lista"<<endl;
+    // a->printList(a);
     
+    //  calcular el tiempo de ejecucion
+    t0=clock();   
+        a->ordenAscendente(a);
+    t1 = clock();
+    /*
+    cout<<"\nLista ordenadada de forma Ascendente"<<endl;
+    a->ordenAscendente(a);
+    a->printList(a);
+    cout<<"\nLista ordenadada de forma Descendente"<<endl;
+    a->ordenDescendente(a);
+    a->printList(a);
+    */
     double time = (double(t1-t0)/CLOCKS_PER_SEC);
-    cout << "Execution Time : " << time << endl;
+    cout << "Tiempo de ejecucion del ordenamiento: " << time << endl;
  
     return 0;
     system("pause");
